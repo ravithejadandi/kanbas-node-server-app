fde08cb2e1f10f4d064d0e81ab2b85ad432d6b91
ravithejadandi <dandi.r@northeastern.edu>
2023-12-13 19:23:53 -0500

    A^

--- app.js
+++ app.js
@@ -8,10 +8,34 @@ import CourseRoutes from './Courses/routes.js';
 import ModuleRoutes from './Modules/routes.js';
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-Hello(app);
-Lab5(app);
-CourseRoutes(app);
-ModuleRoutes(app);
-app.listen(process.env.PORT || 4000);
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    })
+  );
+  const sessionOptions = {
+    secret: 'any string',
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== 'development') {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: 'none',
+      secure: true,
+    };
+  }
+  
+  app.use(session(sessionOptions));
+  
+  app.use(express.json());
+  const CONNECTION_STRING =
+    process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+  mongoose.connect(CONNECTION_STRING);
+  UserRoutes(app);
+  Hello(app);
+  Lab5(app);
+  CourseRoutes(app);
+  ModuleRoutes(app);
+  app.listen(process.env.PORT || 4000);
